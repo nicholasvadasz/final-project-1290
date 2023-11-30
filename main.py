ca3277e21d284c4357169509785e7a59fabb0505
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2023-11-30 13:52:55 -0500

    add arg parsing

---
+++ main.py
@@ -0,0 +1,9 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="CSCI1290 - Final Project")
+    parser.add_argument('-vi', '-video_input', help="Input video file")
+    parser.add_argument('-o', '-output', help="Output video file")
+    parser.add_argument('-fi', '-folder_input', help="Input folder")
+    args = parser.parse_args()
+    print(args)
